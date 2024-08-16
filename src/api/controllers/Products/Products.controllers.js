aa549d7ceaaae3cd6d85a6ecedd818cdd6f2658f
Monique Mendes <moniiquemendes96@gmail.com>
2024-08-16 11:19:32 +0200

    creation of the function to add products and define routes

--- src/api/controllers/Products/Products.controllers.js
+++ src/api/controllers/Products/Products.controllers.js
@@ -1,4 +1,5 @@
 const getAllProductsService = require("../../services/Products/Products.service");
+const productService = require("../../services/Products/Products.service");
 
 const getAllProducts = async (req, res) => {
   try {
@@ -10,4 +11,14 @@ const getAllProducts = async (req, res) => {
   }
 };
 
-module.exports = getAllProducts;
+const addProduct = async (req, res) => {
+  try {
+    const newProduct = await productService.createProduct(req.body);
+    res.status(200).json(newProduct);
+  } catch (error) {
+    console.error("Erro ao adicionar produtos:", error);
+    res.status(404).json({ error: error.menssage });
+  }
+};
+
+module.exports = { getAllProducts, addProduct };
--- src/api/routes/Products.routes.js
+++ src/api/routes/Products.routes.js
@@ -1,8 +1,10 @@
 const ProductsRoutes = require("express").Router();
 const getAllProducts = require("../controllers/Products/products.controllers");
+const addProduct = require("../controllers/Products/products.controllers");
 
 //Rutas
 
 ProductsRoutes.get("/getAllProducts", getAllProducts);
+ProductsRoutes.get("/addProducts", addProduct);
 
 module.exports = ProductsRoutes;
